0d9f4a6b4f6f9e11d90acbdff0741e387a1611dd
Marcelo Sampaio <marcelo@artlogica.com,br>
2018-12-10 08:00:05 -0200

    Initial Commit

---
+++ SwiftQrCodeReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftQrCodeReader
+//
+//  Created by Marcelo Sampaio on 10/12/18.
+//  Copyright © 2018 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
